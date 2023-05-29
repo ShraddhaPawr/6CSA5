ff626701a205829e834f6c247abde599be9b571e
Rushi <Rushi@DESKTOP-VGVGQGQ>
2023-05-29 10:42:30 +0530

    this is my second commit

--- 6CSA5/src/Assignment/CookiePractice.java
+++ 6CSA5/src/Assignment/CookiePractice.java
@@ -1,5 +1,8 @@
 package Assignment;
 
+import java.util.Set;
+
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,6 +10,15 @@ public class CookiePractice {
 public static void main(String[] args) {
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://www.facebook.com");
-	System.err.println(all.Cookies.size());
+	Set<Cookie> allCookies = driver.manage().getCookies();
+	System.out.println(allCookies.size());
+	for(Cookie singleCooky:allCookies)
+		System.out.println(singleCooky.getName());
+	Cookie cooky=new Cookie("selenium","6");
+	driver.manage().addCookie(cooky);
+	allCookies = driver.manage().getCookies();
+	System.out.println(allCookies.size());
+	for(Cookie singleCooky:allCookies)
+		System.out.println(singleCooky.getName());
 }
 }
