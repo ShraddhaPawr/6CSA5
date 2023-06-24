1e682817cd67cd404798747e109336b6a19ea2b3
Rushi <Rushi@DESKTOP-VGVGQGQ>
2023-06-24 08:57:18 +0530

    24-06-23 Commit.

---
+++ 6CSA5/src/JavaScriptExecuter/ScrollBy.java
@@ -0,0 +1,18 @@
+package JavaScriptExecuter;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScrollBy {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.amazon.in");
+	JavascriptExecutor jse=(JavascriptExecutor) driver;
+	for(int i=0;i<=20000;i++)
+	jse.executeScript("window.scrollBy(0,2)");
+	for(int i=0;i<=1000;i++)
+		jse.executeScript("window.scrollBy(0,-2)");
+}
+}
