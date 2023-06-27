70e08f3fc34d6b3fdd50b9fd89f10a76b532c4b2
Rushi <Rushi@DESKTOP-VGVGQGQ>
2023-06-27 19:38:53 +0530

    27-06-23 Commit.

---
+++ 6CSA5/src/Frames/SwitchBack.java
@@ -0,0 +1,21 @@
+package Frames;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SwitchBack {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.get("https://www.selenium.dev/selenium/docs/api/java/index.html?overview-summary.html");
+	driver.switchTo().frame(0);
+	driver.findElement(By.linkText("org.openqa.selenium.bidi.browsingcontext")).click();
+	driver.switchTo().defaultContent();
+	driver.switchTo().frame("packageFrame");
+    driver.findElement(By.linkText("BrowsingContextInfo")).click();
+}
+}
